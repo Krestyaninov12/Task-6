1bdec90d36bef33cad86fcbd6896a794b072c09f
Krestyaninov12 <kreda@pnpe.ru>
2021-11-13 23:18:57 +0300

    Добавьте файлы проекта.

---
+++ Task 6.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Введите строку:\t");
+            string input = Console.ReadLine();
+            string input1 = input.Replace(" ", "");
+            char[] obrinput = input1.ToCharArray();
+            Array.Reverse(obrinput);
+            string finaltext = new string(obrinput);
+            if (input1 == finaltext)
+            {
+                Console.Write("Введенная фраза палиндром");
+            }
+            else
+                Console.Write("Введенная фраза не палиндром");
+                Console.ReadKey(); 
+            }
+                }
+}
---
+++ Task 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int mlength = 0;
+            int identif = 0;
+            int i;
+
+            Console.Write("Введите строку:\t");
+            string input = Console.ReadLine();
+            string[] array = input.Split(new Char[] { ' ' });
+            for (i = 0; i< array.Length; i++)
+            { if (array[i].Length > mlength)
+                {
+                    mlength = array[i].Length;
+                    identif = i;
+                } 
+            }
+            Console.Write("Самое длинное слово в вашей фразе:\t {0}", array[identif]);
+            Console.ReadKey();
+        }
+    }
+}
